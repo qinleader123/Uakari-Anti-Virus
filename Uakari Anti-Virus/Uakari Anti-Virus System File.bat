1d0c95008134c027d97c74a95d7451587cc5a861
Dishonest-Uakari <45131234+qinleader123@users.noreply.github.com>
2018-12-11 15:17:37 -0600

    Update For Uakari Anti-Virus
    Bug Fixes

--- Uakari Anti-Virus/Uakari Anti-Virus System File.bat
+++ Uakari Anti-Virus/Uakari Anti-Virus System File.bat
@@ -6,8 +6,8 @@ title Uakari Anti-Virus
 :menu
 mode 44,10
 cls
-echo Uakari Anti-Virus                       Version 0.6
-echo ___________________________________________________
+echo Uakari Anti-Virus                 Version 0.6
+echo _____________________________________________
 echo 1) PC Clean-Up
 echo 2) Scan For Viruses
 echo 3) List Currently Running Programs
